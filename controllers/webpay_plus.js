d99e96aa6e366937e402bf7d833de5c27ebe5651
Felipe Fiebig <felipe.fiebig@continuum.cl>
2021-01-07 15:57:57 -0300

    Add create

--- controllers/webpay_plus.js
+++ controllers/webpay_plus.js
@@ -1,8 +1,36 @@
-exports.create = function (req, res, next) {
+const WebpayPlus = require("transbank-sdk").WebpayPlus;
+
+exports.create = async function (req, res, next) {
+  let buyOrder = "O-" + Math.floor(Math.random() * 10000) + 1;
+  let sessionId = "S-" + Math.floor(Math.random() * 10000) + 1;
+  let amount = Math.floor(Math.random() * 1000) + 1001;
+  let returnUrl =
+    req.protocol + "://" + req.get("host") + "/webpay_plus/commit";
+
+  let response = await WebpayPlus.Transaction.create(
+    buyOrder,
+    sessionId,
+    amount,
+    returnUrl
+  );
+
+  let token = response.token;
+  let url = response.url;
+
+  let viewData = {
+    buyOrder,
+    sessionId,
+    amount,
+    returnUrl,
+    response,
+    token,
+    url,
+  };
   res.render("webpay_plus/create", {
     step: "Crear Transacción",
     stepDescription:
       "En este paso crearemos la transacción con el objetivo de obtener un identificador unico y " +
       "poder en el siguiente paso redirigir al Tarjetahabiente hacia el formulario de pago",
+    viewData,
   });
 };
--- routes/webpay_plus.js
+++ routes/webpay_plus.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var router = express.Router();
 var webpay_plus_controller = require("../controllers/webpay_plus");
+const WebpayPlus = require("transbank-sdk").WebpayPlus;
+
+router.use(function (req, res, next) {
+  WebpayPlus.configureWebpayPlusForTesting();
+  next();
+});
 
 router.get("/create", webpay_plus_controller.create);
 
