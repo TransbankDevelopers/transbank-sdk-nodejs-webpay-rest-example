30285bc4122f1d916532ca86201573fe096ede22
Felipe Fiebig <felipe.fiebig@continuum.cl>
2021-02-22 18:16:13 -0300

    Add Favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var favicon = require("serve-favicon");
 
 var indexRouter = require("./routes/index");
 var webpayPlusRouter = require("./routes/webpay_plus");
@@ -23,6 +24,7 @@ if (app.settings.env == "development") {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
