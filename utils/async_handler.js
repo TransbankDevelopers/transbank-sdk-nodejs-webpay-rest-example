0be3f500a44ea0d7d14252849bcb9d81c16d89fc
Felipe Fiebig <felipe.fiebig@continuum.cl>
2021-05-10 11:41:22 -0400

    Modify error handler

--- utils/async_handler.js
+++ utils/async_handler.js
@@ -1,5 +1,8 @@
 const asyncHandler = (fn) => (request, response, next) => {
-  return Promise.resolve(fn(request, response, next)).catch(next);
+  return Promise.resolve(fn(request, response, next)).catch((error) => {
+    console.log(error);
+    next();
+  });
 };
 
 module.exports = asyncHandler;
